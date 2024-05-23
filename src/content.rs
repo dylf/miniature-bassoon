c6fabee0468425db474de83e77ff8b3abea91835
Dylan Fontaine <10430358+dylf@users.noreply.github.com>
2024-05-23 07:58:04 -0700

    feat: add more widgets

--- src/content.rs
+++ src/content.rs
@@ -4,11 +4,13 @@ use cosmic::{theme, Element};
 
 pub struct Content {
     input: String,
+    options: Radios,
 }
 
 #[derive(Debug, Clone)]
 pub enum Message {
     Input(String),
+    Radio(Radios),
     Submit,
 }
 
@@ -16,10 +18,18 @@ pub enum Command {
     Save(String),
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum Radios {
+    Option1,
+    Option2,
+    Option3,
+}
+
 impl Content {
     pub fn new() -> Self {
         Self {
-            input: String::new()
+            input: String::new(),
+            options: Radios::Option1,
         }
     }
 
@@ -31,20 +41,32 @@ impl Content {
         let spacing = theme::active().cosmic().spacing;
         widget::button::button(widget::text::text(fl!("save")))
             .on_press(Message::Submit)
-            .padding(spacing.space_xxs)
+            .padding([spacing.space_xxs, spacing.space_m])
             .into()
     }
 
+    fn radios(&self) -> Element<Message> {
+        let spacing = theme::active().cosmic().spacing;
+        let selected = Some(self.options);
+        let r1 = widget::radio::Radio::new("1", Radios::Option1, selected, Message::Radio).size(12);
+        let r2 = widget::radio::Radio::new("2", Radios::Option2, selected, Message::Radio).size(12);
+        let r3 = widget::radio::Radio::new("3", Radios::Option3, selected, Message::Radio).size(12);
+        widget::column().spacing(spacing.space_xxs).push(r1).push(r2).push(r3).into()
+    }
+
     pub fn view(&self) -> Element<Message> {
+        let spacing = theme::active().cosmic().spacing;
         widget::column()
+            .spacing(spacing.space_xs)
             .push(self.title())
+            .push(widget::warning::warning("Warning: I don't know what I'm doing!"))
             .push(
                 widget::text_input::text_input("Enter your name", &self.input)
                     .on_input(Message::Input)
-                    .size(10)
                     .label("Name")
-                    .padding(10),
+                    .width(150)
             )
+            .push(self.radios())
             .push(self.button())
             .into()
     }
@@ -56,6 +78,11 @@ impl Content {
                 None
             }
             Message::Submit => Some(Command::Save(self.input.clone())),
+            Message::Radio(radio) => {
+                self.options = radio;
+                println!("Radio: {:?}", radio);
+                None
+            }
         }
     }
     
